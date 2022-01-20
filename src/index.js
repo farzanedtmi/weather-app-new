7e5d55a655f2cf74d59c7781963931adcd525766
farzanedtmi <96782314+farzanedtmi@users.noreply.github.com>
2022-01-20 18:12:53 +0330

    forecast

--- src/index.js
+++ src/index.js
@@ -23,7 +23,49 @@ let days = [
 ];
 let day = days[now.getDay()];
 
+
+
 saat.innerHTML = `${day} , ${hours}:${minutes}`;
+
+
+function displayForcast (response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row"> `;
+  forecast.forEach( function (forecastDay , index ) {
+    if (index < 6)
+{forecastHTML = forecastHTML + ` <div class= "col-2"> 
+
+<div class= "weather-forecast-date">${formatDay(forecastDay.dt)} </div>}
+<img src ="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png " width="41" />
+<div class="weather-forecast-temperatures">
+<span class = " weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+<span class = " weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
+</div>
+</div>
+`;   } });  
+
+
+
+function formatDay (timestamp){
+  let date = new Date (timestamp*1000);
+  let day = date.getDay();
+  let days = ["Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"]
+  return days [day];
+}
+
+function getForecast (coordinates) {
+
+  let apiKey = "0b121fa36f264f094fd0196401db2f00";
+  let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then (displayForcast);
+}
 
 function displayTemperature (response) {
 
@@ -37,8 +79,11 @@ function displayTemperature (response) {
   document.querySelector("#Wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#Humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  getForecast(response.data.coord);
   
 }
+
+
 function esmeshahr(event) {
   event.preventDefault();
   let apiKey = "0b121fa36f264f094fd0196401db2f00";
@@ -84,5 +129,3 @@ fahrenheitLink.addEventListener("click", displayFarenhideTepmerature);
 
 let celsiusLink = document.querySelector("#celcious");
 celsiusLink.addEventListener("click", displaycelsiousTepmerature);
-
-esmeshahr (Tokyo);
