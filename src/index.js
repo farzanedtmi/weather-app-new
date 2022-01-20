9c4b9250564bd9baef7fae0e5fa70377776c4674
farzanedtmi <96782314+farzanedtmi@users.noreply.github.com>
2022-01-20 18:22:14 +0330

    changes

--- src/index.js
+++ src/index.js
@@ -32,12 +32,12 @@ function displayForcast (response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row"> `;
-  forecast.forEach( function (forecastDay , index ) {
+  forecast.forEach(function (forecastDay , index ) {
     if (index < 6)
 {forecastHTML = forecastHTML + ` <div class= "col-2"> 
 
-<div class= "weather-forecast-date">${formatDay(forecastDay.dt)} </div>}
-<img src ="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png " width="41" />
+<div class= "weather-forecast-date">${formatDay(forecastDay.dt)} </div>
+<img src = "http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" width="41" />
 <div class="weather-forecast-temperatures">
 <span class = " weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
 <span class = " weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
@@ -129,3 +129,5 @@ fahrenheitLink.addEventListener("click", displayFarenhideTepmerature);
 
 let celsiusLink = document.querySelector("#celcious");
 celsiusLink.addEventListener("click", displaycelsiousTepmerature);
+
+
